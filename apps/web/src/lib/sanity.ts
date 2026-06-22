4a0c16850312c5a682f9ef8feb702f0ad0714ea0
Smit Mistry <smitmistry19@gmail.com>
2026-06-22 16:00:27 +0530

    useCdn

--- apps/web/src/lib/sanity.ts
+++ apps/web/src/lib/sanity.ts
@@ -3,4 +3,5 @@ import { configureSanity } from "@repo/sanity";
 export const { sanityFetch } = configureSanity({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  useCdn: import.meta.env.PROD,
 });
